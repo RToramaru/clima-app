6cdaaf70001a023152e91928479c4afd3407936a
rafael almeida <rafael.almeida.soares2012@gmail.com>
2022-05-14 23:50:58 -0300

    criacao de uma classe para conectar na api

---
+++ lib/helpers/api_helper.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class ApiHelper {
+  late String url;
+
+  ApiHelper(this.url);
+
+  Future getDados() async {
+    try {
+      http.Response response =
+          await http.get(Uri.parse(url)).timeout(const Duration(seconds: 5));
+      if (response.statusCode == 200) {
+        String dados = response.body;
+        var dadosJson = jsonDecode(dados);
+        return dadosJson;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      // ignore: avoid_print
+      print('Erro $e');
+    }
+  }
+}
