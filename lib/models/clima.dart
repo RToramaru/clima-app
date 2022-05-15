8ef5f18f495a12ae680f2da38d89e0c88afd754b
rafael almeida <rafael.almeida.soares2012@gmail.com>
2022-05-15 20:57:10 -0300

    retirar obrigatoriedade

--- lib/models/clima.dart
+++ lib/models/clima.dart
@@ -1,22 +1,22 @@
 class Clima {
   final String cidade;
   final num condicao;
-  final num temperadura;
+  final num temperatura;
   final num vento;
   final num umidade;
 
   Clima(
-      {required this.cidade,
-      required this.condicao,
-      required this.temperadura,
-      required this.vento,
-      required this.umidade});
+      { this.cidade,
+       this.condicao,
+       this.temperatura,
+       this.vento,
+       this.umidade});
 
   factory Clima.fromJson(Map<String, dynamic> json) {
     return Clima(
       cidade: json['name'],
       condicao: json['weather'][0]['id'],
-      temperadura: json['main']['temp'],
+      temperatura: json['main']['temp'],
       vento: json['wind']['speed'],
       umidade: json['main']['humidity'],
     );
